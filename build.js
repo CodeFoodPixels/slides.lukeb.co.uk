dee8cb51d96a6e7eb06187f9492970655952cdef
Luke Bonaccorsi <luke.bonaccorsi@skybettingandgaming.com>
2019-01-04 16:21:33 +0000

    Add redirect list to 404 page

--- build.js
+++ build.js
@@ -3,11 +3,20 @@
 const fs = require('fs');
 const redirects = require('./redirects.json');
 
-fs.mkdirSync(`build`);
-
-fs.copyFileSync(`404.html`, `build/404.html`);
-
-for (let slug in redirects) {
-    fs.appendFileSync(`build/_redirects`, `/${slug} ${redirects[slug]} 301!\n`);
+if (fs.existsSync(`build`)) {
+    fs.rmdirSync(`build`);
 }
 
+fs.mkdirSync(`build`);
+
+const links = [];
+for (let slug in redirects) {
+    fs.appendFileSync(`build/_redirects`, `/${slug.toLowerCase()} ${redirects[slug]} 301!\n`);
+    links.unshift(`<p><a href="${redirects[slug]}">${slug}</a></p>\n`);
+}
+
+let errorPage = fs.readFileSync(`404.html`, 'utf8');
+
+errorPage = errorPage.replace(`{{list}}`, links.join(''));
+
+fs.writeFileSync(`build/404.html`, errorPage);
